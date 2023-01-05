38214694849439e3f36699f4495b1abbaacfa8e7
Michael Kenny <git@mmkenny.net>
2023-01-05 15:04:28 +1030

    Exports selected apps to apps.json.
    It just appends on the end of the app array. If you run it again it will add duplicates.

--- SunshineAppExport.psm1
+++ SunshineAppExport.psm1
@@ -1,50 +1,52 @@
-function OnApplicationStarted()
-{
-    $__logger.Info("OnApplicationStarted")
+function GetMainMenuItems {
+    param(
+        $getMainMenuItemsArgs
+    )
+
+    $menuItem1 = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
+    $menuItem1.Description = "Export selected games"
+    $menuItem1.FunctionName = "SunshineExport"
+    $menuItem1.MenuSection = "@Sunshine App Export"
+    
+    return $menuItem1
 }
 
-function OnApplicationStopped()
-{
-    $__logger.Info("OnApplicationStopped")
-}
+function SunshineExport {
+    param(
+        $scriptMainMenuItemActionArgs
+    )
 
-function OnLibraryUpdated()
-{
-    $__logger.Info("OnLibraryUpdated")
-}
+    # Set creation counter
+    $shortcutsCreatedCount = 0
 
-function OnGameStarting()
-{
-    param($evnArgs)
-    $__logger.Info("OnGameStarting $($evnArgs.Game)")
-}
+    $appsPath = "$Env:ProgramW6432\Sunshine\apps.json"
+    $json = ConvertFrom-Json (Get-Content $appsPath -Raw)
 
-function OnGameStarted()
-{
-    param($evnArgs)
-    $__logger.Info("OnGameStarted $($evnArgs.Game)")
-}
+    foreach ($game in $PlayniteApi.MainView.SelectedGames) {
+        $gameLaunchURI = 'playnite://playnite/start/' + "$($game.id)"
 
-function OnGameStopped()
-{
-    param($evnArgs)
-    $__logger.Info("OnGameStopped $($evnArgs.Game) $($evnArgs.ElapsedSeconds)")
-}
+        $logOutput = "$($game.name.ToLower().Replace(' ', '_')).log"
 
-function OnGameInstalled()
-{
-    param($evnArgs)
-    $__logger.Info("OnGameInstalled $($evnArgs.Game)")
-}
+        if ($null -ne $game.CoverImage) {
 
-function OnGameUninstalled()
-{
-    param($evnArgs)
-    $__logger.Info("OnGameUninstalled $($evnArgs.Game)")
-}
+            $sourceCover = $PlayniteApi.Database.GetFullFilePath($game.CoverImage)
+            if (($game.CoverImage -notmatch "^http") -and (Test-Path $sourceCover -PathType Leaf)) {
 
-function OnGameSelected()
-{
-    param($gameSelectionEventArgs)
-    $__logger.Info("OnGameSelected $($gameSelectionEventArgs.OldValue) -> $($gameSelectionEventArgs.NewValue)")
+                $newApp = New-Object -TypeName psobject
+                Add-Member -InputObject $newApp -MemberType NoteProperty -Name "name" -Value $game.name
+                Add-Member -InputObject $newApp -MemberType NoteProperty -Name "output" -Value $logOutput
+                Add-Member -InputObject $newApp -MemberType NoteProperty -Name "cmd" -Value $gameLaunchURI
+                Add-Member -InputObject $newApp -MemberType NoteProperty -Name "image-path" -Value $sourceCover
+
+                $json.apps += $newApp
+            }
+        }
+
+        $shortcutsCreatedCount++
+    }
+
+    ConvertTo-Json $json -Depth 100 | Out-File $appsPath -Encoding utf8
+
+    # Show finish dialogue with shortcut creation count
+    $PlayniteApi.Dialogs.ShowMessage(("Sunshine app shortcuts created: {0}" -f $shortcutsCreatedCount), "Sunshine App Export")
 }
