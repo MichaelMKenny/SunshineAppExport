MichaelMKenny/SunshineAppExport

commit 1ce4c1a67af8f536be4bd98c698e89ed1c7aa381

Use game.id instead of gameLaunchCmd for deciding if to update existing app or add a new entry.
This stops the script from adding dupes of all the user's existing apps, after b48f11a adds quotes to gameLaunchCmd.

SunshineAppExport.psm1
@@ -128,6 +128,15 @@ function SunshineExport {
     $window.ShowDialog()
 }
 
+function GetGameIdFromCmd([string]$cmd) {
+    $parts = $cmd.Split(" --start ")
+    if ($parts.Count -gt 1) {
+        return $parts[1].Split(" ")[0]
+    } else {
+        return ""
+    }
+}
+
 function doWork([string]$appsPath) {
     # Load assemblies
     Add-Type -AssemblyName System.Drawing
@@ -196,7 +205,7 @@ function doWork([string]$appsPath) {
 
                 $json.apps = $json.apps | ForEach-Object {
                     if ($_.detached) {
-                        if ($_.detached[0] -eq $gameLaunchCmd) {
+                        if (GetGameIdFromCmd($_.detached[0]) -eq $game.id) {
                             $newApp
                         }
                         else {
@@ -210,7 +219,7 @@ function doWork([string]$appsPath) {
 
                 if (!($json.apps | Where-Object { 
                             if ($_.detached) {
-                                return $_.detached[0] -eq $gameLaunchCmd
+                                return GetGameIdFromCmd($_.detached[0]) -eq $game.id
                             }
                             else {
                                 return $false

commit bbe65f4454a7d131b5d99b528a3b0a5448eeceaf

Fixed regression caused by 1ce4c1a, which made the script only able to export 1 game.
Closes #11.

SunshineAppExport.psm1
@@ -129,9 +129,9 @@ function SunshineExport {
 }
 
 function GetGameIdFromCmd([string]$cmd) {
-    $parts = $cmd.Split(" --start ")
+    $parts = $cmd -split " --start "
     if ($parts.Count -gt 1) {
-        return $parts[1].Split(" ")[0]
+        return ($parts[1] -split " ")[0]
     }
     else {
         return ""
@@ -206,7 +206,8 @@ function DoWork([string]$appsPath) {
 
                 $json.apps = $json.apps | ForEach-Object {
                     if ($_.detached) {
-                        if (GetGameIdFromCmd($_.detached[0]) -eq $game.id) {
+                        $gameId = GetGameIdFromCmd($_.detached[0])
+                        if ($gameId -eq $game.id) {
                             $newApp
                         }
                         else {
@@ -220,7 +221,8 @@ function DoWork([string]$appsPath) {
 
                 if (!($json.apps | Where-Object { 
                             if ($_.detached) {
-                                return GetGameIdFromCmd($_.detached[0]) -eq $game.id
+                                $gameId = GetGameIdFromCmd($_.detached[0])
+                                return $gameId -eq $game.id
                             }
                             else {
                                 return $false